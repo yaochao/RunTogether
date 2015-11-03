1137d7089ba23a67a4a64689965f2d256827b9b3
yaochao <yaochao365@qq.com>
2015-11-03 22:17:16 +0800

    网络请求的安全策略修改(关于SSL)

--- RunTogether/Classes/Tools/Network/RTHTTPSessionManager.m
+++ RunTogether/Classes/Tools/Network/RTHTTPSessionManager.m
@@ -22,14 +22,19 @@
         // 设置反序列化的数据格式－> 官方建议的修改方式
         manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", @"text/plain", nil];
         
-        // 设置安全策略，忽略签名证书，SSL
-        AFSecurityPolicy *securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
+        // 设置安全策略，关于自制的签名证书，SSL
+        // 方式一：证书绑定方式
+        AFSecurityPolicy *securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModePublicKey];
+        securityPolicy.allowInvalidCertificates = YES;
         NSString *cerPath = [[NSBundle mainBundle] pathForResource:@"rt" ofType:@"der"];
         NSData *certData = [NSData dataWithContentsOfFile:cerPath];
-        [securityPolicy setAllowInvalidCertificates:YES];
-        [securityPolicy setValidatesDomainName:NO];
         [securityPolicy setPinnedCertificates:@[certData]];
         manager.securityPolicy = securityPolicy;
+        // 方式二：直接忽略，会遭到中间人攻击的危险(建议方式一)
+//        manager.securityPolicy.allowInvalidCertificates = YES;
+//        manager.securityPolicy.validatesDomainName = NO;
+        
+        
     });
     return manager;
 }
