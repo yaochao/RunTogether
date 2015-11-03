e8cf79b48fece6a7090a763fe43131d656f7c268
yaochao <yaochao365@qq.com>
2015-11-03 17:36:46 +0800

    登录接口调试成功

--- RunTogether/Classes/UI/Profile/Login/RTLoginController.m
+++ RunTogether/Classes/UI/Profile/Login/RTLoginController.m
@@ -11,14 +11,38 @@
 @interface RTLoginController ()
 @property (weak, nonatomic) IBOutlet UITextField *username;
 @property (weak, nonatomic) IBOutlet UITextField *password;
-
+@property (strong, nonatomic) NSDictionary *responseObject;
 @end
 
 @implementation RTLoginController
 
 #pragma mark - btnClick
 - (IBAction)loginBtnClick:(id)sender {
+    // 判空
+    if ([_username.text isEqual:@""]) {
+        [MBProgressHUD showError:@"请输入手机号码"];
+        return;
+    }
     
+    if ([_password.text isEqual:@""]) {
+        [MBProgressHUD showError:@"请输入密码"];
+        return;
+    }
+    
+    // 请求网络
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"phone"] = _username.text;
+    params[@"password"] = _password.text;
+  
+    [MBProgressHUD showMessage:@"请稍后..."];
+    [RTNetworkTools postDataWithParams:params interfaceType:@"sessions" success:^(NSDictionary *responseObject) {
+        [MBProgressHUD hideHUD];
+        NSLog(@"%@", responseObject);
+        _responseObject = responseObject;
+        [MBProgressHUD showSuccess:@"登录成功,欢迎你"];
+    } failure:^(NSError *error) {
+        [MBProgressHUD hideHUD];
+    }];
 }
 
 
