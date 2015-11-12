5eadfe2f01b3ce00b6c7b2b24213978b0e0d4884
yaochao <yaochao365@qq.com>
2015-11-12 16:31:25 +0800

    添加定时器，定时上传用户坐标

--- RunTogether/Classes/UI/Location/RTLocationController.mm
+++ RunTogether/Classes/UI/Location/RTLocationController.mm
@@ -22,6 +22,7 @@
 @property (nonatomic, strong) BMKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UILabel *coordinateLbl;
 @property (nonatomic, strong) RTLocationModel *locationModel;
+@property (nonatomic, strong) NSTimer *timer;
 @end
 
 @implementation RTLocationController
@@ -35,24 +36,38 @@
     [RTNotificationCenter addObserver:self selector:@selector(receivedLocationNotification:) name:@"LocationSuccessNotification" object:nil];
 }
 
+#pragma mark - 初始化一个定时器 NSTimer
+- (void)initTimer {
+    // 初始化之前，先废除原来的
+    [self invalidateTimer];
+    // 初始化
+    NSTimeInterval timeInterval = 10.0;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:timeInterval target:self selector:@selector(updateLocation) userInfo:nil repeats:YES];
+}
 
-#pragma mark - ----
+#pragma mark - 废除定时器
+- (void)invalidateTimer {
+    if ([self.timer isValid]) {
+        [self.timer invalidate];
+    }
+    self.timer = nil;
+}
+
+#pragma mark - 收到坐标变化的通知
 - (void)receivedLocationNotification:(NSNotification *)notification {
     //去除HUD
     [MBProgressHUD hideHUD];
     _locationModel = notification.userInfo[@"LocationSuccessKey"];
     // 更新地理坐标显示
     _coordinateLbl.text = [NSString stringWithFormat:@"%f - %f", _locationModel.point.latitude, _locationModel.point.longitude];
-    // 上传到服务器，每次收到都上传
-    [self updateLocation:_locationModel];
 }
 
 
 #pragma mark - 上传用户地理位置到服务器
-- (BOOL)updateLocation:(RTLocationModel *)locationModel {
+- (BOOL)updateLocation {
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
-    params[@"latitude"] = @(locationModel.point.latitude);
-    params[@"longitude"] = @(locationModel.point.longitude);
+    params[@"latitude"] = @(self.locationModel.point.latitude);
+    params[@"longitude"] = @(self.locationModel.point.longitude);
     NSString *interface = [NSString stringWithFormat:@"users/%@/locations", [RTKeyChainTools getUserId]];
     [RTNetworkTools postDataWithParams:params interfaceType:interface success:^(NSDictionary *responseObject) {
         NSLog(@"更新用户位置 - %@", responseObject);
@@ -68,6 +83,7 @@
 - (IBAction)startLocationBtnClick:(id)sender {
     [MBProgressHUD showMessage:@"正在开启定位..."];
     [RTLocationTools startLocation];
+    [self initTimer];
 }
 
 - (IBAction)stopLocationBtnClick:(id)sender {
@@ -75,6 +91,7 @@
     [RTLocationTools stopLocation];
     _coordinateLbl.text = @"请开启定位";
     [MBProgressHUD hideHUD];
+    [self invalidateTimer];
 }
 
 
