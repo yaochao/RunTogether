4e175ceeb8a2c7f02c5dd5461baed341c4dd21a3
yaochao <yaochao365@qq.com>
2015-11-12 15:42:02 +0800

    把网络错误的处理抛给实际的代码

--- RunTogether/Classes/Tools/Network/RTNetworkTools.m
+++ RunTogether/Classes/Tools/Network/RTNetworkTools.m
@@ -13,10 +13,7 @@
 
 #pragma mark - POST 加载网络数据
 + (void)postDataWithParams:(NSMutableDictionary *)params interfaceType:(NSString *)interfaceType success:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure {
-    
-    // loadcookies
-//    [self loadCookies];
-    
+
     // 获得网络管理单例对象
     RTHTTPSessionManager *manager = [RTHTTPSessionManager sharedNetworkManager];
     if (params == nil) {
@@ -29,21 +26,10 @@
     
     // 发送POST请求
     [manager POST:completeUrl parameters:params success:^(NSURLSessionDataTask *task, id responseObject) {
-        
         success(responseObject);
-        // savecookies
-//        [self saveCookies];
-        
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
         failure(error);
-        NSLog(@"网络错误 - %@", error);
-        // 提示网络有问题
-        NSString *errorMsg = [NSString stringWithFormat:@"请检查您的网络连接\n错误代码 %li", error.code];
-        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"网络错误" message:errorMsg delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
-        [alertView show];
     }];
-    
-    
 }
 
 
