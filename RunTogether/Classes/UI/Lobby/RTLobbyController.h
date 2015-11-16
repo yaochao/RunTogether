cbfa52c5cfe2deb234c0479bf7f7e4d07d7a2bc5
yaochao <yaochao365@qq.com>
2015-11-16 20:05:17 +0800

    add

---
+++ RunTogether/Classes/UI/Lobby/RTLobbyController.h
@@ -0,0 +1,13 @@
+//
+//  RTLobbyController.h
+//  RunTogether
+//
+//  Created by yaochao on 15/11/16.
+//  Copyright © 2015年 duoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTLobbyController : UIViewController
+
+@end
