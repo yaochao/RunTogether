0810bd0ff2f67e0cb0c5d95be89e4df9a8c2e347
yaochao <yaochao365@qq.com>
2015-11-17 10:30:37 +0800

    fix

--- RunTogether/Classes/UI/Lobby/RTLobbyController.m
+++ RunTogether/Classes/UI/Lobby/RTLobbyController.m
@@ -7,9 +7,13 @@
 //
 
 #import "RTLobbyController.h"
+#import "NSObject+UserDefaults.h"
 
-@interface RTLobbyController ()
+#define dataSource  @[@1, @3, @5, @10]
 
+@interface RTLobbyController () <UIPickerViewDataSource, UIPickerViewDelegate>
+@property (nonatomic, strong) NSArray *data;
+@property (nonatomic, copy) NSString *isLogin;
 @end
 
 @implementation RTLobbyController
@@ -17,27 +21,44 @@
 #pragma mark - viewDidLoad
 - (void)viewDidLoad {
     [super viewDidLoad];
+    // 数据源
+    self.data = dataSource;
 }
 
-- (IBAction)goClick:(UIButton *)sender {
-    
+
+#pragma mark - datasource
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
 }
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.data.count;
+}
+
 
 #pragma mark - delegate
-// returns the number of 'columns' to display.
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
-    
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return [NSString stringWithFormat:@"%@ km", self.data[row]];
 }
 
-// returns the # of rows in each component..
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    NSLog(@"select %li row", (long)row);
 }
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component {
+    return 50;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)goClick:(UIButton *)sender {
+    NSLog(@"go");
+}
+
 
 
 @end
