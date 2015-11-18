cd0907bdc06b05293dce35a1948c103785734ff5
yaochao <yaochao365@qq.com>
2015-11-18 16:26:06 +0800

    fix gitignore

---
+++ RunTogether/Classes/BussinessModels/Push/RTLocationCreatedModel.h
@@ -0,0 +1,13 @@
+//
+//  RTLocationCreatedModel.h
+//  RunTogether
+//
+//  Created by yaochao on 15/11/18.
+//  Copyright © 2015年 duoduo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RTLocationCreatedModel : NSObject
+
+@end
---
+++ RunTogether/Classes/BussinessModels/Push/RTLocationCreatedModel.m
@@ -0,0 +1,13 @@
+//
+//  RTLocationCreatedModel.m
+//  RunTogether
+//
+//  Created by yaochao on 15/11/18.
+//  Copyright © 2015年 duoduo. All rights reserved.
+//
+
+#import "RTLocationCreatedModel.h"
+
+@implementation RTLocationCreatedModel
+
+@end
