275335dbc338952b99326ccdf9a95a2ef0aa594d
zhaohuan <zhaohuan@duoduosports.com>
2015-11-27 11:37:39 +0800

    save user property

---
+++ RunTogether/Classes/UI/Profile/RTAvatarVoiceViewController.m
@@ -0,0 +1,27 @@
+//
+//  RTAvatarVoiceViewController.m
+//  RunTogether
+//
+//  Created by 赵欢 on 15/11/27.
+//  Copyright © 2015年 duoduo. All rights reserved.
+//
+
+#import "RTAvatarVoiceViewController.h"
+
+@interface RTAvatarVoiceViewController ()
+
+@end
+
+@implementation RTAvatarVoiceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
