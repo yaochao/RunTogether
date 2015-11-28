26b5f879854b15a8c97ab1825b961c6fe38fb70b
yaochao <yaochao365@qq.com>
2015-11-29 00:40:08 +0800

    add:BlurEffect

---
+++ RunTogether/Classes/UI/RTBlurEffectController.h
@@ -0,0 +1,13 @@
+//
+//  RTBlurEffectController.h
+//  RunTogether
+//
+//  Created by yaochao on 15/11/28.
+//  Copyright © 2015年 duoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTBlurEffectController : UITableViewController
+
+@end
