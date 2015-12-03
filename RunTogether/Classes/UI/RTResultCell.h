5b2a7c710c04ddf7da4fc845f9ea65d13abe70d3
yaochao <yaochao365@qq.com>
2015-12-03 19:00:01 +0800

    add 结果页

---
+++ RunTogether/Classes/UI/RTResultCell.h
@@ -0,0 +1,13 @@
+//
+//  RTResultCell.h
+//  RunTogether
+//
+//  Created by yaochao on 15/12/3.
+//  Copyright © 2015年 duoduo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTResultCell : UITableViewCell
+
+@end
---
+++ RunTogether/Classes/UI/RTResultController.m
@@ -0,0 +1,37 @@
+//
+//  RTResultController.m
+//  RunTogether
+//
+//  Created by yaochao on 15/12/3.
+//  Copyright © 2015年 duoduo. All rights reserved.
+//
+
+#import "RTResultController.h"
+
+@interface RTResultController ()
+
+@end
+
+@implementation RTResultController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
